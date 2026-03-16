695c1418dce5dd2cdabd9c849ca7ba5547b4defa
tomasz_ciesielczyk <tomaszc@man.poznan.pl>
2026-03-16 15:52:37 +0100

    update swagger

--- entso-e/tm_entso_e/core/healthcheck/router.py
+++ entso-e/tm_entso_e/core/healthcheck/router.py
@@ -5,17 +5,17 @@ from tm_entso_e.core.healthcheck import service
 router = APIRouter(prefix="")
 
 
-@router.get("/")
+@router.get("/", description="check if service is responding")
 async def status():
     from effi_onto_tools.utils import time_utils
     return time_utils.current_timestamp()
 
 
-@router.get("/state")
+@router.get("/state",description="Determine service state")
 async def state():
     return service.get_service_state()
 
 
-@router.get("/report")
+@router.get("/report",description="Detailed service state")
 async def report():
     return service.get_service_report()
